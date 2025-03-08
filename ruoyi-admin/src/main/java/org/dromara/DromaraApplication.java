54d426e63c0f5e299da76524f50d2909afdc1a56
yixiaco <co627428179@gmail.com>
2025-03-08 18:58:03 +0800

    pref: 添加启动耗时

--- ruoyi-admin/src/main/java/org/dromara/DromaraApplication.java
+++ ruoyi-admin/src/main/java/org/dromara/DromaraApplication.java
@@ -4,6 +4,7 @@ import cn.hutool.crypto.SecureUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+import org.springframework.util.StopWatch;
 
 /**
  * 启动程序
@@ -15,11 +16,18 @@ import org.springframework.boot.context.metrics.buffering.BufferingApplicationSt
 public class DromaraApplication {
 
     public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        // 开始计时
+        stopWatch.start();
+
         SecureUtil.disableBouncyCastle();
         SpringApplication application = new SpringApplication(DromaraApplication.class);
         application.setApplicationStartup(new BufferingApplicationStartup(2048));
         application.run(args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  ruoyi-tdesign启动成功   ლ(´ڡ`ლ)ﾞ");
+
+        // 停止计时
+        stopWatch.stop();
+        System.out.printf("(♥◠‿◠)ﾉﾞ  ruoyi-tdesign启动成功, 共耗时：%.3f秒   ლ(´ڡ`ლ)ﾞ\n", stopWatch.getTotalTimeSeconds());
     }
 
 }
