2d07ad1a9197cff0dc5ac8523360ab776debe849
yixiaco <co627428179@gmail.com>
2025-03-14 01:08:46 +0800

    update 优化通过角色ID查询用户逻辑

--- ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/service/impl/SysUserServiceImpl.java
+++ ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/service/impl/SysUserServiceImpl.java
@@ -51,7 +51,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 /**
  * 用户 业务层处理
@@ -696,10 +696,9 @@ public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> impl
             new LambdaQueryWrapper<SysUserRole>().in(SysUserRole::getRoleId, roleIds));
 
         // 获取用户ID列表
-        List<Long> userIds = userRoles.stream()
-            .map(SysUserRole::getUserId).distinct().collect(Collectors.toList());
+        Set<Long> userIds = StreamUtils.toSet(userRoles, SysUserRole::getUserId);
 
-        return selectListByIds(userIds);
+        return selectListByIds(new ArrayList<>(userIds));
     }
 
     /**
